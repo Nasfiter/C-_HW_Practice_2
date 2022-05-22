04e233740d3d7021e417540b7e61e062151d158d
Alex Nejen <alex_nas@Mac-mini-Alex.local>
2022-05-21 20:38:57 -0400

    home work 3 is done

---
+++ HW_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+//int number = new Random().Next();
+//Console.WriteLine($"Число = {number}");
+
+Console.Write("Введи число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+var NumberText = Convert.ToString(Number);
+if (NumberText.Length > 2)
+{
+    Console.Write("третья цифра = ");
+    Console.Write(NumberText[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
