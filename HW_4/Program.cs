19e3735debcbb45bb611ecbc0f2eabe52a9bc8ac
Alex Nejen <alex_nas@Mac-mini-Alex.local>
2022-05-21 20:06:16 -0400

    HW 4 is done

---
+++ HW_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 15: Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+// Примеры:
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.Write("Введите число соответствующее дню неделит: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void Checking  (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+  else { 
+    Console.WriteLine("это точно не выходной!");
+  }
+}
+
+Checking(dayNumber);
